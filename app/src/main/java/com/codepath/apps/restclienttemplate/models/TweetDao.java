5aa4fa92a2186b4b48e378af9c960ecd61b54d7e
Jessica Jumelles <jessicajumelles@gmail.com>
2021-09-29 22:08:32 -0400

    Updating and commiting new files

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
@@ -0,0 +1,23 @@
+package com.codepath.apps.restclienttemplate.models;
+
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.OnConflictStrategy;
+
+import java.util.List;
+
+@Dao
+public interface TweetDao {
+
+    @Query("SELECT Tweet.body AS tweet_body, Tweet.createAt AS tweet_createdAt, Tweet.id AS tweet_id, User.*" +
+            "FROM Tweet INNER JOIN User ON Tweet.userID = User.id ORDER BY createAt DESC LIMIT 5")
+    List<TweetWithUser> recentItems();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertModel(Tweet... tweets);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertModel(User... users);
+}
